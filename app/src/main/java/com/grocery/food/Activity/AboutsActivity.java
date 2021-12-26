3451136cbf9135e8275471dc47e3a2da0f321df3
Irvan Muhandis <irvanmuhandis@gmail.com>
2021-12-26 09:56:38 +0700

    bener

--- app/src/main/java/com/grocery/food/Activity/AboutsActivity.java
+++ app/src/main/java/com/grocery/food/Activity/AboutsActivity.java
@@ -1,6 +1,8 @@
 package com.grocery.food.Activity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.grocery.food.R;
 
@@ -10,5 +12,9 @@ public class AboutsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abouts);
+
+        ImageView sled = findViewById(R.id.sledsow);
+        AnimationDrawable sow = (AnimationDrawable) sled.getDrawable();
+        sow.start();
     }
 }
