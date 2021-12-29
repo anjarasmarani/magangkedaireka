19cbc2f87fe7dfcaed466d7de29928e7ed279a0b
Irvan Muhandis <irvanmuhandis@gmail.com>
2021-12-29 23:49:14 +0700

    transfer beres

---
+++ app/src/main/java/com/grocery/food/Activity/BuktiTf.java
@@ -0,0 +1,14 @@
+package com.grocery.food.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuktiTf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bukti_tf);
+    }
+}
